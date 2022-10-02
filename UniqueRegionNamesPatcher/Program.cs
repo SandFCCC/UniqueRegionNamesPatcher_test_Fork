ddd8198ca01932f16ebb0ca5eced8623cc9aaf70
SandFCCC <83374591+SandFCCC@users.noreply.github.com>
2022-10-02 18:25:04 +0200

    Update Program.cs

--- UniqueRegionNamesPatcher/Program.cs
+++ UniqueRegionNamesPatcher/Program.cs
@@ -73,7 +73,8 @@ namespace UniqueRegionNamesPatcher
             InitializeHandlers(state);
 
             int totalChanges = 0;
-            var wrldGetter_WinningContextOverrides = state.LoadOrder.ListedOrder.Worldspace().WinningContextOverrides().Record;
+            var wrldGetter_WinningContextOverrides = state.LoadOrder.ListedOrder.Worldspace().WinningContextOverrides();//.Record;
+            Console.WriteLine(wrldGetter_WinningContextOverrides)
             foreach (var wrldGetter in wrldGetter_WinningContextOverrides)
             {
                 if (GetHandlerForWorldspace(wrldGetter) is WorldspaceProcessor handler)
